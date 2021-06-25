1da83faa2c25ac91ff9dfbcd706499a43ff4d29b
andrewEichinger <andreweichinger21@gmail.com>
2021-06-25 19:02:03 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+
+// https:api.themoviedb.org/3/movie/550?api_key=3951aaaa350c1d4bbe3275a095820e70
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Is The Book Better?</h1>
       </header>
     </div>
   );
