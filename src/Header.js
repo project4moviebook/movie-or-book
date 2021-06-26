23179b31b343e13054101a25af75b569185de3e4
andrewEichinger <andreweichinger21@gmail.com>
2021-06-25 22:07:09 -0400

    got search working, redid grid

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,12 @@
 
 // Our header for our webpage
 
-const Header = function () {
+const Header = function (props) {
+
     return (
         <header className="App-header">
             <h1>Is The Book Better?</h1>
-            <form action="">
+            <form onSubmit={(e) => props.search(e)} action="">
                 <label className="sr-only" htmlFor="submit">Enter a Movie/Book</label>
                 <input type="text" name="submit" id="submit" placeholder="Enter a Movie/Book" />
                 <input type="submit" value="Submit" />
